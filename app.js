4d0d14ed04dc3bf5eb6633c5b55384db5eb565a5
dehacker <robitsegay2018@gmail.com>
2022-12-18 23:30:25 -0800

    socket implemented with peer server

--- app.js
+++ app.js
@@ -28,7 +28,15 @@ const options = {
 const server = require('https').createServer(options,app);
 const server1 = require('http').createServer(app);
 const io = require('socket.io')(server);
+const chat = require('./routes/chat');
+const post = require('./routes/post');
+const notification = require('./routes/notification');
+// const socket = require('./socket');
 
+const Chat = require('./model/chats');
+const User = require('./model/users');
+const Post = require('./model/post');
+const Notification = require('./model/notification');
 const {
     ExpressPeerServer
 } = require('peer');
@@ -37,7 +45,142 @@ const peerServer = ExpressPeerServer(server, {
 });
 
 
+io.on('connection', (socket) => {
+    socket.emit('msg', 'welcome to our app'),
+        socket.on('join-room', (roomId, userId) => {
+            socket.join(roomId); // Join the room
+            socket.broadcast.emit('user-connected', userId); // Tell everyone else in the room that we joine
+            console.log('a user connected');
+            socket.on('disconnect',()=>{
+                socket.to(roomId).broadcast.emit('user-disconnected',userId)
+            })
+        });
+        socket.on('videocalling', (msg) => {
+            io.emit('video',msg)
+        }
+        )
+        socket.on('audiocalling', (msg) => {
+            io.emit('audio',msg)
+        }
+        )
 
+    // });
+    socket.on('inputmsg', (msg) => {
+        var d = new Date();
+        var n = d.getTime();  
+        const mamsg = {
+            userfrom:msg.userfrom,
+            userto:msg.userto,
+            message:msg.message,
+            type:msg.type,
+            date:n
+        }
+         console.log(mamsg)
+
+        const chat = new Chat(mamsg);
+
+        chat
+            .save()
+            .then(() => {
+                User.find({
+                    _id: msg.userfrom
+                }).then((user) => {
+                    msg.userfrom = user[0];
+                    console.log('are u there');
+                    return io.emit('outputmsg', msg);
+                });
+            })
+            .catch(() => {});
+    });
+    socket.on('post', (userpost) => {
+        User.find({
+            _id: userpost.userid
+        }).then((data) => {
+            userpost.userid = data[0];
+
+            const post = new Post(userpost);
+
+            post
+                .save()
+                .then((doc) => {
+                    console.log('are u there');
+                    return io.emit('changepost', doc);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        });
+    });
+    socket.on('comment', (comm) => {
+        Post.findOneAndUpdate({
+                _id: comm.postid
+            },
+
+            {
+                $push: {
+                    comments: [{
+                        username: comm.name,
+                        comment: comm.comment,
+                        useravatar: comm.avatar,
+                    }, ],
+                },
+            }, {
+                new: true
+            }
+        ).then(() => {
+            console.log('this is  a comment');
+            // res.json(post);
+        });
+    });
+    socket.on('like', (like) => {
+        Post.findOneAndUpdate({
+                _id: like.postid
+            },
+
+            {
+                $push: {
+                    likes: [{
+                        like: like.userid
+                    }],
+                },
+            }, {
+                new: true
+            }
+        ).then(() => {
+            console.log('this is  a like');
+        });
+    });
+    socket.on('dislike', (like) => {
+        Post.findOneAndUpdate({
+                    _id: like.postid
+                },
+
+                {
+                    $pop: {
+                        likes: [{
+                            like: like.userid
+                        }],
+                    },
+                }, {
+                    new: true
+                }
+            )
+            .then(() => {
+                console.log('this is  dislike');
+            })
+            .catch((err) => {
+                console.log('error dislike ' + err);
+            });
+    });
+    socket.on('notificaton', (not) => {
+        const no = new Notification(not);
+        no.save()
+            .then(console.log('success'))
+            .catch((err) => {
+                console.log(err);
+            });
+    });
+});
 
 app.use(morgan('dev'));
 app.use(express.json());
@@ -83,8 +226,8 @@ function Errorhandler(error, req, res, next) {
 }
 
 
-app.use(notFound);
-app.use(Errorhandler);
+//app.use(notFound);
+//app.use(Errorhandler);
 mongoose
     .connect('mongodb://localhost:27017/tyes', {
         useNewUrlParser: true,
@@ -95,3 +238,5 @@ server.listen(port, () => {
     console.log(` app started on a port ${port}`);
 });
 
+// TODO: Creating user name update
+// FIXME: profile pic upload and make it there
